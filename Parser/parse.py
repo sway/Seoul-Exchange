3041be84c73c4a622781c4937aff1f9ffd2ad61f
Honza Ustohal <honza@egoistic.biz>
2011-11-12 21:32:38 +0900

    Layout changes, stuff...

--- Parser/parse.py
+++ Parser/parse.py
@@ -4,6 +4,7 @@ from pytz import timezone
 from lxml import etree
 
 kst = timezone("Asia/Seoul")
+gmt = timezone("GMT")
 
 def processPost(post):
 	title = post.findtext("title").encode('utf8')
@@ -11,20 +12,23 @@ def processPost(post):
 	dateStr = dateStr[:-6]
 	
 	dt_gmt = datetime.datetime.strptime(dateStr, "%a, %d %b %Y %H:%M:%S")
-	dt_gmt = dt_gmt.replace(tzinfo=timezone("GMT"))
+	dt_gmt = dt_gmt.replace(tzinfo=gmt)
 
 	dt = dt_gmt.astimezone(kst)
 	
 	filename = dt.strftime("%Y-%m-%d-%H-%M-%S") + '.tex'
 	
-	content = post.findtext("content_encoded")	
+	content = post.findtext("content_encoded").encode('utf8')
 	
 	f = open("data/" + filename, "w")
 	
 	f.write('\\begin{post}\n')
 	f.write('\t\\posttitle{' + title + '}\n')
 	f.write('\t\\postdate{' + str(dt.year) + '}{' + str(dt.month) + '}{' + str(dt.day) + '}{' + str(dt.hour) + '}{' + str(dt.minute) + '}{' + str(dt.second) + '}\n')
-	
+	f.write('\t\\begin{content}\n')
+	f.write(content + '\n')
+	f.write('\t\\end{content}\n')
+	f.write('\\end{post}\n')
 	f.close()
 	
 if len(sys.argv) < 2:
